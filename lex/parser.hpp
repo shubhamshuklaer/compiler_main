05590f70cf596e3328cab8623cbc29a32fc3ac1b
Kunal Khandelwal <kunal15595@gmail.com>
2015-04-11 14:14:31 +0530

    type doing

--- lex/parser.hpp
+++ lex/parser.hpp
@@ -122,7 +122,7 @@ extern int yydebug;
 typedef union YYSTYPE YYSTYPE;
 union YYSTYPE
 {
-#line 57 "parser.y" /* yacc.c:1909  */
+#line 60 "parser.y" /* yacc.c:1909  */
 
 	struct node *entry;
 	char *terminal_value;
